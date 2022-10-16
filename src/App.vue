c4b97d4910ffebf4d382b9fc506d9bae840703b4
Daniel <danielabdi12@gmail.com>
2022-10-17 01:06:26 +0200

    Initial commit. Project setup

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import Navigation from "./components/Navigation.vue"
+  import Hero from "./components/Hero.vue"
+  import Testimonial from "./components/Testimonial.vue"
+  import Reasons from "./components/Reasons.vue"
+  import Interview from "./components/Interview.vue"
+  import Video from "./components/Video.vue"
+  import Examples from "./components/Examples.vue"
+  import Articles from "./components/Articles.vue"
+  import Creators from "./components/Creators.vue"
+  import Footer from "./components/Footer.vue"
+</script>
+
+<template>
+  <!-- Navigation -->
+  <Navigation />
+  <!-- Hero -->
+  <Hero />
+  <!-- Testimonial -->
+  <Testimonial />
+  <!-- Reasons to use UX Laws -->
+  <Reasons />
+  <!-- Interview about UX in gaming -->
+  <Interview />
+  <!-- Youtube video about the 19 design principles -->
+  <Video />
+  <!-- Examples of UX -->
+  <Examples />
+  <!-- Articles about pratical UX Laws -->
+  <Articles />
+  <!-- Creators recognition -->
+  <Creators />
+  <!-- Footer -->
+  <Footer />
+</template>
+
+<style scoped></style>
