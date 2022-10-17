6e054fa981a349f0297096971e81c5244a6b777c
Daniel <danielabdi12@gmail.com>
2022-10-17 11:24:00 +0200

    Add scorll to hero to entire logo

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -8,9 +8,9 @@
         <a class="mr-5 hover:text-gray-900 cursor-pointer" v-scroll-to="'#Examples'">Examples</a>
         <a class="hover:text-gray-900 cursor-pointer" v-scroll-to="'#Articles'">Articles</a>
         </nav>
-        <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
-    <img src="../assets/images/logopng.png" alt="UX Laws" width="75" height="75"/>
-        <span class="ml-3 text-xl cursor-pointer" v-scroll-to="'#Hero'">DaSi</span>
+        <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0 cursor-pointer" v-scroll-to="'#Hero'">
+            <img src="../assets/images/logopng.png" alt="UX Laws" width="75" height="75"/>
+            <span class="ml-3 text-xl">DaSi</span>
         </a>
         <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
         <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Github
