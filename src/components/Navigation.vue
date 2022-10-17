dee729eb0eace698986f339ab1ec7f9801487543
Daniel <danielabdi12@gmail.com>
2022-10-17 20:03:19 +0200

    Add active scroll link

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,30 +1,134 @@
 <template>
-    <header class="text-gray-600 body-font sticky top-0 bg-color z-50">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-        <a class="mr-5 hover:text-gray-900 nav-link cursor-pointer" v-scroll-to="'#Hero'"><span class="intro">Intro</span></a>
-        <a class="mr-5 hover:text-gray-900 nav-link cursor-pointer" v-scroll-to="'#Interview'"><span class="interview">Interview</span></a>
-        <a class="mr-5 hover:text-gray-900 nav-link cursor-pointer" v-scroll-to="'#Video'"><span class="video">Video</span></a>
-        <a class="mr-5 hover:text-gray-900 nav-link cursor-pointer" v-scroll-to="'#Examples'"><span class="examples">Examples</span></a>
-        <a class="hover:text-gray-900 nav-link cursor-pointer" v-scroll-to="'#Articles'"><span class="articles">Articles</span></a>
-        </nav>
-        <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0 cursor-pointer" v-scroll-to="'#Hero'">
-            <img src="../assets/images/logopng.png" alt="UX Laws" width="75" height="75"/>
-            <span class="ml-3 text-xl">DaSi</span>
-        </a>
-        <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Github
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+  <header class="text-gray-600 body-font sticky top-0 bg-color z-50">
+    <div
+      class="
+        container
+        mx-auto
+        flex flex-wrap
+        p-5
+        flex-col
+        md:flex-row
+        items-center
+      "
+    >
+      <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+        <a
+          class="intro Hero mr-5 hover:text-gray-900 nav-link cursor-pointer"
+          v-scroll-to="'#Hero'"
+          ><span class="intro Hero">Intro</span></a
+        >
+        <a
+          class="mr-5 hover:text-gray-900 nav-link cursor-pointer"
+          v-scroll-to="'#Interview'"
+          ><span class="interview Interview">Interview</span></a
+        >
+        <a
+          class="mr-5 hover:text-gray-900 nav-link cursor-pointer"
+          v-scroll-to="'#Video'"
+          ><span class="video Video">Video</span></a
+        >
+        <a
+          class="mr-5 hover:text-gray-900 nav-link cursor-pointer"
+          v-scroll-to="'#Examples'"
+          ><span class="examples Examples">Examples</span></a
+        >
+        <a
+          class="hover:text-gray-900 nav-link cursor-pointer"
+          v-scroll-to="'#Articles'"
+          ><span class="articles Articles">Articles</span></a
+        >
+      </nav>
+      <a
+        class="
+          flex
+          order-first
+          lg:order-none lg:w-1/5
+          title-font
+          font-medium
+          items-center
+          text-gray-900
+          lg:items-center lg:justify-center
+          mb-4
+          md:mb-0
+          cursor-pointer
+        "
+        v-scroll-to="'#Hero'"
+      >
+        <img
+          src="../assets/images/logopng.png"
+          alt="UX Laws"
+          width="75"
+          height="75"
+        />
+        <span class="ml-3 text-xl">DaSi</span>
+      </a>
+      <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+        <button
+          class="
+            inline-flex
+            items-center
+            bg-gray-100
+            border-0
+            py-1
+            px-3
+            focus:outline-none
+            hover:bg-gray-200
+            rounded
+            text-base
+            mt-4
+            md:mt-0
+          "
+        >
+          Github
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
+          </svg>
         </button>
-        </div>
+      </div>
     </div>
-    </header>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "Navigation"
-}
+  name: "Navigation",
+  data() {
+    return {
+      sections: document.querySelectorAll("section"),
+      navLi: document.querySelectorAll("nav"),
+    };
+  },
+  mounted() {
+    window.onscroll = () => {
+      const sections = document.querySelectorAll("section");
+      const navItems = document.querySelectorAll("nav a span");
+      let current = "";
+
+      console.log(sections, "section");
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 350) {
+          current = section.getAttribute("id");
+        }
+      });
+
+      navItems.forEach((a) => {
+        a.classList.remove("active");
+        console.log(current, "A");
+        if (a.classList.contains(current)) {
+          a.classList.add("active");
+        }
+      });
+    };
+  },
+};
 </script>
